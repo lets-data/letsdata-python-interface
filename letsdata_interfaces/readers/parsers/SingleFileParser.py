dc5c396be706bc7e1981d61d1d4b00da5e3355e9
Usman Shami <usmanshami@gmail.com>
2023-12-14 09:20:35 -0800

    Revert "Example implementations of LetsData interfaces in python"
    This reverts commit ed30ce6a3e864aaa80ddd7c3b4e46d0a4240197b.

--- letsdata_interfaces/readers/parsers/SingleFileParser.py
+++ letsdata_interfaces/readers/parsers/SingleFileParser.py
@@ -5,126 +5,161 @@ from letsdata_interfaces.readers.model.ParseDocumentResult import ParseDocumentR
 from letsdata_interfaces.documents.Document import Document
 from letsdata_interfaces.documents.ErrorDoc import ErrorDoc
 from letsdata_utils.logging_utils import logger
-from letsdata_utils.validations import letsdata_assert
-from letsdata_interfaces.documents.DocumentType import DocumentType
-import re
-from enum import Enum
-from datetime import datetime
-from collections import namedtuple
-
 '''
     The parser interface for Single File Reader usecase. This is used when all the files are of a single type and the records in the file do not follow a state machine.
     This interface is where you tell us how to parse the individual records from the file. Since this is single file reader, there is no state machine maintained.
-    
-    This implementation parses the 'conversion' records from the Common Crawl Web Extraction Template (WET) files.
+    Simple example is a log file where each line is a data record and the extracted document is transformed from each data record (line in the file).
 
-                Example: WET Files Record Layout:
-                ---------------------------------
+                Example: Single File Reader Files and Record Layout:
 
-                'warcinfo' WARCINFO Header
-                WARCINFO Payload
-
-                'conversion' WARC Header
-                'conversion' Payload
-
-                'conversion' WARC Header
-                'conversion' Payload
-
-                'conversion' WARC Header
-                'conversion' Payload
-
-                ...
+                +----------------------+------------------=---+---------------------+
+                | logfile_1.gz         | logfile_2.gz         | logfile_3.gz        |
+                +---=------------------+----------------------+---------------------+
+                | <logline_1>          | <logline_1>          | <logline_1>         |
+                | <logline_2>          | <logline_2>          | <logline_2>         |
+                | <logline_3>          | <logline_3>          | <logline_3>         |
+                | <logline_4>          | <logline_4>          | <logline_4>         |
+                | ...                  | ...                  | ...                 |
+                +----------------------+----------------------+---------------------+
  '''
 class SingleFileParser:
     def __init__(self) -> None:
         pass 
 
     '''
-     * The filetype of the file - for the example we've used, we define the filetype (logical name) as "WET"
-     * 
+     * The filetype of the file - for the example we've used, we define the filetype (logical name) as "LOGFILE"
+     * Here is an example implementation:
+     *
+     *     public String getS3FileType() {
+     *         return "LOGFILE"
+     *     }
+     *
      * @return - The file type
      '''
     def getS3FileType(self) -> str:
-        return "WET"
+        raise(Exception("Not Yet Implemented"))
 
     '''
-     * Given the filetype (WET) and filename (crawl-data/CC-MAIN-2022-21/segments/1652662509990.19/wet/CC-MAIN-20220516041337-20220516071337-00000.warc.wet.gz) from the manifest file, return the resolved filename if necessary.
+     * Given the filetype (LOGLINE) and filename (logfile_1.gz/logfile_2.gz/logfile_3.gz) from the manifest file, return the resolved filename if necessary.
      * In most cases, the resolved filename would be the same as the filename, but in some cases, you might need to prepend paths if the data in the s3 bucket is not in the root directory.
-     * In this example, the manifest filename is fully qualified so we return it as is.
+     * For example, dataset_name/log_date/logfile_1.gz (See the docs for MultipleFileStateMachineReader.getResolvedS3Filename method for additional details around the file name resolution)
      *
-     * @param s3FileType - the file type - example WET
-     * @param fileName - the file name - example crawl-data/CC-MAIN-2022-21/segments/1652662509990.19/wet/CC-MAIN-20220516041337-20220516071337-00000.warc.wet.gz
-     * @return - the resolved file name - example crawl-data/CC-MAIN-2022-21/segments/1652662509990.19/wet/CC-MAIN-20220516041337-20220516071337-00000.warc.wet.gz
+     * Here is an example implementation using the example in the class doc:
+     *
+     *      public String getResolvedS3FileName(String s3FileType, String fileName) {
+     *          ValidationUtils.validateAssertCondition(getS3FileType().equals(fileType.toUpperCase()), "fileType is unexpected");
+     *          ValidationUtils.validateAssertCondition(fileName != null, "filename should not be null");
+     *          // no resolution being done, expect incoming file names to be resolved but custom logic can be put here
+     *          return resolvedFileNames;
+     *      }
+     *
+     * @param s3FileType - the file type - example LOGFILE
+     * @param fileName - the file name - example logfile_1.gz
+     * @return - the resolved file name - example dataset_name/data_date/logfile_1.gz
      '''
     def getResolvedS3FileName(self, s3FileType : str,  fileName : str) -> str:
-       return fileName
+       raise(Exception("Not Yet Implemented"))
 
 
     '''
      * The record start pattern - to extract the records from the file, the parser needs to know the record start delimiter - it will search the file sequentially till it finds this delimiter and then from that point on, it will search for the end record delimiter.
      * Once it finds the end record delimiter, it will copy those bytes to the parse document function to create an extracted record
      *
-     *      For example, here is an abbreviated WET file:
+     *      For example, for the following log lines in the log file, we define the start pattern as "{\"ts\"" and end pattern as "}\n":
      *
-     *      WET File:
-     *      ---------
-     * 
-     *      WARC/1.0
-     *      WARC-Type: conversion                                                   <--- we use this string as the start pattern
-     *      WARC-Target-URI: http://023hrk.com/a/chanpinzhongxin/cp2/104.html
-     *      WARC-Date: 2022-05-16T04:40:56Z
-     *      WARC-Record-ID: <urn:uuid:f8a15ad2-9d24-42d6-8f7a-7d9431246752>
-     *      WARC-Refers-To: <urn:uuid:5232afac-fb10-401c-b522-445db8bdbf2a>
-     *      WARC-Block-Digest: sha1:M7TRGS3KUALFLHMGLJSDMER3FDP2Z2Q4
-     *      WARC-Identified-Content-Language: zho
-     *      Content-Type: text/plain
-     *      Content-Length: 3710
-     *      
-     *      漫步肩关节_重庆鸿瑞铠体育设施有限公司
-     *      网站地图
-     *      加入收藏
-     *      联系我们
-     *      您好！欢迎访问重庆鸿瑞铠体育设施有限公司！
-     *      优质环保原料
-     *      更环保更安全
-     *      施工保障
-     *      流程严谨、匠心工艺
-     *      使用年限
-     *      高出平均寿命30%
-     *      全国咨询热线
-     *      ...
-     *                                                                              <--- we use '\n\r\n\r\n' as the end pattern
+     *      Logfile:
+     *      --------
+     *       {"ts":1647352053448,"dt":"Mar 15, 2022 6:47:33 AM","hnm":"archimedes-mbp-2.hsd1.wa.comcast.net","unm":"archimedes","lvl":"WARN","thd":"main","cnm":"com.ancient.mathematicians.archimedes.InvalidBuoyantForceException","fnm":"InvalidBuoyantForceException.java","lnm":178,"mnm":"validateArchimedesPrinciple","msg":"The buoyant force is different from the weight. Archimedes' principle has been invalidated."}
+     *       {"ts":1647352053449,"dt":"Mar 15, 2022 6:47:33 AM","hnm":"archimedes-mbp-2.hsd1.wa.comcast.net","unm":"archimedes","lvl":"WARN","thd":"main","cnm":"com.ancient.mathematicians.archimedes.InvalidBuoyantForceException","fnm":"InvalidBuoyantForceException.java","lnm":178,"mnm":"validateArchimedesPrinciple","msg":"The buoyant force is different from the weight. Archimedes' principle has been invalidated."}
      *
-     *      WARC/1.0
-     *      WARC-Type: conversion
-     *      ...
-     *
-     *
+     *       Example implementation:
+     *       -----------------------
+     *       public RecordParseHint getRecordStartPattern(String s3FileType) {
+     *           ValidationUtils.validateAssertCondition(getS3FileType().equals(fileType.toUpperCase()), "fileType is unexpected");
+     *           return new RecordParseHint(RecordHintType.PATTERN, "{\"ts\"", -1);
+     *       }
      *
      * @param s3FileType - the filetype
      * @return - the record start pattern as a RecordParseHint object
      '''
     def getRecordStartPattern(self, s3FileType : str) -> RecordParseHint:
-        return RecordParseHint(RecordHintType.PATTERN, "WARC-Type: conversion", -1)
+        raise(Exception("Not Yet Implemented"))
         
     
     '''
      * The record end pattern - to extract the records from the file, the parser needs to know the record start delimiter - it will search the file sequentially till it finds this delimiter and then from that point on, it will search for the end record delimiter.
      * Once it finds the end record delimiter, it will copy those bytes to the parse document function to create an extracted record
      *
-     * See above for file structure. We use '\n\r\n\r\n' as the end pattern
+     *      For example, for the following log lines in the log file, we define the start pattern as "{\"ts\"" and end pattern as "}\n":
+     *
+     *      Logfile:
+     *      --------
+     *       {"ts":1647352053448,"dt":"Mar 15, 2022 6:47:33 AM","hnm":"archimedes-mbp-2.hsd1.wa.comcast.net","unm":"archimedes","lvl":"WARN","thd":"main","cnm":"com.ancient.mathematicians.archimedes.InvalidBuoyantForceException","fnm":"InvalidBuoyantForceException.java","lnm":178,"mnm":"validateArchimedesPrinciple","msg":"The buoyant force is different from the weight. Archimedes' principle has been invalidated."}
+     *       {"ts":1647352053449,"dt":"Mar 15, 2022 6:47:33 AM","hnm":"archimedes-mbp-2.hsd1.wa.comcast.net","unm":"archimedes","lvl":"WARN","thd":"main","cnm":"com.ancient.mathematicians.archimedes.InvalidBuoyantForceException","fnm":"InvalidBuoyantForceException.java","lnm":178,"mnm":"validateArchimedesPrinciple","msg":"The buoyant force is different from the weight. Archimedes' principle has been invalidated."}
+     *
+     *       Example implementation:
+     *       -----------------------
+     *       public RecordParseHint getRecordEndPattern(String s3FileType) {
+     *           ValidationUtils.validateAssertCondition(getS3FileType().equals(fileType.toUpperCase()), "fileType is unexpected");
+     *           return new RecordParseHint(RecordHintType.PATTERN, "}\n", -1);
+     *       }
      * @param s3FileType - the filetype
      * @return - the record end pattern as a RecordParseHint object
      '''
     def getRecordEndPattern(self, s3FileType : str) -> RecordParseHint:
-        return RecordParseHint(RecordHintType.PATTERN, "\n\r\n\r\n", -1)
+        raise(Exception("Not Yet Implemented"))
     
     '''
      *  This function is called with the document contents in a byteArr and the startIndex and endIndex into the byteArr as the start and end of the record.
      *  The implementer is expected to construct the output record from these bytes.
      *
-     *  The example implementation below parses the header key value pairs, extract the url as the docId, partitionKey and other headers as document attributes.
-     *  We also extract the document text and return these as a document with Success status code.
+     *  Here is an example implementation which parses error records from the file:
+     *  @Override
+     *  public ParseDocumentResult parseDocument(String s3FileType, String s3Filename, long offsetBytes, byte[] byteArr, int startIndex, int endIndex) {
+     *      ValidationUtils.validateAssertCondition(s3FileType != null && s3FileType.equalsIgnoreCase(ParserFileType), "ResonanceJsonLogProcessor.parseDocument file type is unexpected");
+     *      ValidationUtils.validateAssertCondition(byteArr != null && startIndex >= 0 && byteArr.length > endIndex && endIndex > startIndex, "ResonanceJsonLogProcessor.parseDocument byte array offsets are invalid");
+     *
+     *      // uses the Matcher utility from the interface package to do pattern matching
+     *      int errorLevelIndex = Matcher.match(byteArr, startIndex, endIndex, "\"lvl\":\"ERROR\"");
+     *      if (errorLevelIndex == -1) {
+     *          // skip since we are interested only in ERROR records
+     *          Map<String, String> startOffset = new HashMap<>();
+     *          startOffset.put(s3FileType, Long.toString(offsetBytes));
+     *          Map<String, String> endOffset = new HashMap<>();
+     *          endOffset.put(s3FileType, Long.toString(offsetBytes));
+     *          String errorMessage = "skipping message - level is not ERROR";
+     *          String documentId = null;
+     *          String recordType = null;
+     *          Map<String, Object> documentMetadata = null;
+     *          String serialize = null;
+     *          String partitionKey = s3Filename;
+     *          DocumentInterface skipDoc = new SkipDoc(startOffset, endOffset, errorMessage, documentId, recordType, documentMetadata, serialize, partitionKey);
+     *          return new ParseDocumentResult(null, skipDoc, ParseDocumentResultStatus.SKIP);
+     *      } else {
+     *          try {
+     *              Map<String, Object> jsonMap = objectMapper.readValue(byteArr, startIndex, endIndex, HashMap.class);
+     *              Long timestamp = (Long) jsonMap.get("ts");
+     *              String docId = ""+timestamp+s3Filename;
+     *              String recordType = (String) jsonMap.get("lvl");
+     *              DocumentInterface doc = new JsonLogDocument(docId, recordType, null,  objectMapper.writeValueAsString(jsonMap), docId);
+     *              return new ParseDocumentResult(null, doc, ParseDocumentResultStatus.SUCCESS);
+     *          } catch (Exception e) {
+     *              logger.error("error processing json document from file", s3FileType, s3Filename, offsetBytes, e);
+     *              Map<String, String> startOffset = new HashMap<>();
+     *              startOffset.put(s3FileType, Long.toString(offsetBytes));
+     *              Map<String, String> endOffset = new HashMap<>();
+     *              endOffset.put(s3FileType, Long.toString(offsetBytes));
+     *              String errorMessage = "error processing json document from file - ex: "+e;
+     *              String documentId = null;
+     *              String recordType = null;
+     *              Map<String, Object> documentMetadata = null;
+     *              String serialize = null;
+     *              String partitionKey = s3Filename;
+     *              DocumentInterface errorDoc = new ErrorDoc(startOffset, endOffset, errorMessage, documentId, recordType, documentMetadata, serialize, partitionKey);
+     *              return new ParseDocumentResult(null, errorDoc, ParseDocumentResultStatus.ERROR);
+     *          }
+     *      }
+     *  }
      *
      * @param s3FileType - the filetype
      * @param s3Filename - the filename
@@ -135,73 +170,5 @@ class SingleFileParser:
      * @return - ParseDocumentResult which has the extracted record and the status (error, success or skip)
      '''
     def parseDocument(self, s3FileType : str, s3Filename : str, offsetBytes : int , byteArr : bytearray, startIndex : int, endIndex : int) -> ParseDocumentResult:
-        headerEndIndex = byteArr.find(bytearray('\r\n\r\n', encoding="utf-8"), startIndex, endIndex)
-        if headerEndIndex == -1:
-            raise RuntimeError(f"Header end index not found in Http Response - start index: {startIndex}, end index: {endIndex}")
-
-        headerMap = {}
-        currIndex = startIndex
-        while currIndex < headerEndIndex:
-            logger.debug("loop iter - currIndex: "+str(currIndex)+", headerEndIndex: "+str(headerEndIndex))
-            lineEndIndex = byteArr.find(bytearray('\r\n', encoding="utf-8"), currIndex, headerEndIndex)
-            if lineEndIndex <= currIndex:
-                logger.debug("lineEndIndex not found when parsing headers")
-                break
-            
-            line = byteArr[currIndex:lineEndIndex].decode('utf-8').strip()
-            logger.debug("parsed header line "+line)
-            if line is None or line == "" or line.startswith("WARC/1.0"):
-                currIndex = lineEndIndex+len(bytearray('\r\n', encoding="utf-8"))
-            else:
-                letsdata_assert(line.index(':') > 0, "invalid header line")
-                keyValuePair = line.split(': ')
-                headerKey = keyValuePair[0].strip()
-                headerValue = keyValuePair[1].strip()
-                logger.debug("parsed headerKey: "+headerKey+", headerValue: "+headerValue)
-                headerMap[headerKey] = headerValue
-                currIndex = lineEndIndex+len(bytearray('\r\n', encoding="utf-8"))
+        raise(Exception("Not Yet Implemented"))
         
-        logger.debug("parsed headerMap: "+str(headerMap))
-
-        docKeyValueMap = {}
-
-        currIndex = headerEndIndex
-        docText = byteArr[currIndex:endIndex].decode('utf-8').strip()
-        letsdata_assert(len(docText) > 0, "invalid docText")
-        
-        docKeyValueMap['docText'] = docText
-
-        url = headerMap['WARC-Target-URI']
-        letsdata_assert(len(url) > 0, "invalid url")
-        docKeyValueMap['url'] = url
-        docKeyValueMap['docId'] = url
-        docKeyValueMap['partitionKey'] = url
-
-        language = None
-        if 'WARC-Identified-Content-Language' in headerMap.keys():
-            language = headerMap['WARC-Identified-Content-Language']
-            docKeyValueMap['language'] = language
-
-        contentType = None
-        if 'Content-Type' in headerMap.keys():
-            contentType = headerMap['Content-Type']
-            docKeyValueMap['contentType'] = contentType
-
-        contentLength = None
-        if 'Content-Length' in headerMap.keys():
-            contentLength = headerMap['Content-Length']
-            docKeyValueMap['contentLength'] = contentLength
-
-        blockDigest = None
-        if 'WARC-Block-Digest' in headerMap.keys():
-            blockDigest = headerMap['WARC-Block-Digest']
-            docKeyValueMap['blockDigest'] = blockDigest
-
-        warcDate = None
-        if 'WARC-Date' in headerMap.keys():
-            warcDate = headerMap['WARC-Date']
-            docKeyValueMap['warcDate'] = warcDate
-
-
-        return ParseDocumentResult(None, Document(DocumentType.Document, url, "Content", url, {}, docKeyValueMap), ParseDocumentResultStatus.SUCCESS)
-    
--- letsdata_interfaces/readers/sagemaker/SagemakerVectorsInterface.py
+++ letsdata_interfaces/readers/sagemaker/SagemakerVectorsInterface.py
@@ -29,7 +29,10 @@ class SagemakerVectorsInterface:
    *
    * The extract function will return only the fields that need vectorization with friendly names that can be used to identify different vectors. Here is an example output:
    *      {
-   * 	        "docText": "Article: US inflation means families are spending more than two years ago. The typical American household spent more in July than they did two years ago to buy the same goods and services, according to recent surveys. That figure underscores the cumulative impact high inflation has had on consumer finances — even as price growth has cooled considerably in recent months."
+   *          "PageTitle": "Breaking News, Latest News and Videos | CNN",
+   * 	        "PageDescription": "View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.com.",
+   * 	        "PageKeywords": "cnn news, daily news, breaking news, news today, current events",
+   * 	        "PageText": "Article: US inflation means families are spending more than two years ago. The typical American household spent more in July than they did two years ago to buy the same goods and services, according to recent surveys. That figure underscores the cumulative impact high inflation has had on consumer finances — even as price growth has cooled considerably in recent months."
    *      }
    *
    * @param documentInterface - the letsdata_interfaces.documents.Document 
@@ -37,9 +40,7 @@ class SagemakerVectorsInterface:
    */
    '''
    def extractDocumentElementsForVectorization(self, document) -> {str : str}:
-        docMap = {}
-        docMap['docText'] = document['docText']
-        return docMap
+        raise(Exception("Not Yet Implemented"))
 
    '''
     /**
@@ -52,11 +53,11 @@ class SagemakerVectorsInterface:
      *      documentInterface:
      *      {
      *          "id": "318792f5-8bcb-4a77-b362-087520efb49c",
-     * 	    "url": "www.cnn.com",
-     * 	    "title": "Breaking News, Latest News and Videos | CNN",
-     * 	    "description": "View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.com.",
-     * 	    "keywords": "cnn news, daily news, breaking news, news today, current events",
-     * 	    "docText": "Article: US inflation means families are spending more than two years ago. The typical American household spent more in July than they did two years ago to buy the same goods and services, according to recent surveys. That figure underscores the cumulative impact high inflation has had on consumer finances — even as price growth has cooled considerably in recent months."
+     * 	        "url": "www.cnn.com",
+     * 	        "title": "Breaking News, Latest News and Videos | CNN",
+     * 	        "description": "View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.com.",
+     * 	        "keywords": "cnn news, daily news, breaking news, news today, current events",
+     * 	        "docText": "Article: US inflation means families are spending more than two years ago. The typical American household spent more in July than they did two years ago to buy the same goods and services, according to recent surveys. That figure underscores the cumulative impact high inflation has had on consumer finances — even as price growth has cooled considerably in recent months."
      *      }
      *
      *      vectorsMap:
@@ -89,12 +90,10 @@ class SagemakerVectorsInterface:
      *      {
      *          "id": "318792f5-8bcb-4a77-b362-087520efb49c",
      *          "url": "www.cnn.com",
-     *          "title": "Breaking News, Latest News and Videos | CNN",
-     *          "description": "View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN.com.",
-     *          "keywords": "cnn news, daily news, breaking news, news today, current events",
-     *          "vectors": {
-     *              "docText": [96.3, 79.8, 18.2, 37.8, 85.6, 83.6, 68.3, 54.5, 95.9, 64, 32.6, 36.4, 68, 93.1, 40.2, 61, 98.2, 52.7, 26.2, 31.1, 26.9, 35.2, 16.8, 25.3, 97.7, 81.6, 87.9, 21.5, 72.4, 12.1, 27.4, 83.9, 81.9, 60.1, 5.5]
-     *          }
+     *          "PageTitle": [58.7, 45.6, 59.1, 75.1] ,
+     *          "PageDescription": [27.6, 12, 9.8, 19, 96, 1.2, 11.3, 9.8, 29.8, 22.3, 22.4, 82.9, 27.5, 12.4, 11.5, 80.3, 30.4],
+     *          "PageKeywords": [31.8, 48.4, 2.4, 98.3, 28.4, 55.5, 15.3, 79.3, 81, 12, 56.4, 56, 13.7, 46.4, 2.9, 46.5, 65.2, 70.7, 13.1, 59.8, 63.8, 22.4, 15.2]
+     *	        "PageText": [96.3, 79.8, 18.2, 37.8, 85.6, 83.6, 68.3, 54.5, 95.9, 64, 32.6, 36.4, 68, 93.1, 40.2, 61, 98.2, 52.7, 26.2, 31.1, 26.9, 35.2, 16.8, 25.3, 97.7, 81.6, 87.9, 21.5, 72.4, 12.1, 27.4, 83.9, 81.9, 60.1, 5.5]
      *      }
      *
      * @param documentInterface - the feature doc as letsdata_interfaces.documents.Document
@@ -103,8 +102,4 @@ class SagemakerVectorsInterface:
      */
    '''
    def constructVectorDoc(self, documentInterface, vectorsMap) -> Document:
-      docKeyValues = documentInterface
-      docKeyValues.pop('docText')
-      docKeyValues['vectors'] = {}
-      docKeyValues['vectors']['docText'] = vectorsMap['docText']
-      return Document(DocumentType.Document, documentInterface['docId'], "Vectors", documentInterface['partitionKey'], documentInterface['documentMetadata'], docKeyValues)
+      raise(Exception("Not Yet Implemented"))
